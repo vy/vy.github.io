2d9d2dc4f1e3974cc917683d3d67affccf0db689
Volkan Yazıcı <volkan.yazici@gmail.com>
2015-01-30 17:11:17 +0100

    Add zlib1g-dev dependency to Vagrant provisioning script.

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,6 +2,6 @@
 
 locale-gen UTF-8
 apt-get update
-apt-get install -y python-pip ruby-dev
+apt-get install -y zlib1g-dev python-pip ruby-dev
 pip install Pygments
 gem install nanoc nanoc-toolbox builder kramdown pygments.rb sass adsf
