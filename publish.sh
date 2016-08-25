976a3472e86e797022499e8c1b01c1efc3cc890c
Volkan Yazici <vyazici@bol.com>
2016-08-25 23:27:18 +0200

    Highlight publish.sh logs.

--- publish.sh
+++ publish.sh
@@ -10,27 +10,27 @@ getLastCommitId() {
 	git log -1 | head -n 1 | awk '{print $2}'
 }
 
-echo "Checking repository state... "
+echo "*** Checking repository state... "
 isEverythingComitted || {
 	echo "error: there are still changes to be committed." >&2
 	exit 1
 }
 lastCommitId=$(getLastCommitId)
 
-echo "Switching to master..."
+echo "*** Switching to master..."
 git checkout master
 
-echo "Syncing output files..."
+echo "*** Syncing output files..."
 rsync --exclude-from rsync-excludes.txt -a --delete --force output/ .
 
-echo "Adding all output files to the repository..."
+echo "*** Adding all output files to the repository..."
 git add .
 
-echo "Committing changes..."
+echo "*** Committing changes..."
 git commit -a -m "Copied changes from source/$lastCommitId."
 
-echo "Pushing changes..."
+echo "*** Pushing changes..."
 git push -f origin master source
 
-echo "Switching back to source..."
+echo "*** Switching back to source..."
 git checkout source
